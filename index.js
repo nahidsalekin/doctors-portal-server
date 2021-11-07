420153d858a1b90eb8295e3bd41308bc7e4a1781
Nahid Salekin <36966165+nahidsalekin@users.noreply.github.com>
2021-11-07 21:56:05 +0600

    jwt added

--- index.js
+++ index.js
@@ -7,12 +7,36 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require('./doctors-portal-sdk.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 //middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fiuyj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodeUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodeUser.email;
+        }
+        catch {
+
+        }
+    }
+    next();
+}
 
 async function run() {
     try {
@@ -38,15 +62,26 @@ async function run() {
             res.json(result);
         });
 
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true
+            }
+            res.json({ admin: isAdmin })
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
+            res.json(result)
 
         });
 
         app.put('/users', async (req, res) => {
             const user = req.body;
-            console.log(user)
             const filter = { email: user.email }
             const options = { upsert: true }
             const updateDoc = { $set: user }
@@ -54,7 +89,26 @@ async function run() {
             res.json(result)
         });
 
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            console.log(req.decodedEmail)
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const user = req.body;
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result)
+                }
+            }
+            else {
+                res.status(403)
+            }
 
+
+
+        })
 
 
 
